e7c8337f87ff73390a4212a7a848f6b63f611b43
developer-shourav <mrinmoyraj823@gmail.com>
2024-08-02 14:57:48 +0600

    Loading and Error page added

---
+++ app/loading.tsx
@@ -0,0 +1,8 @@
+export default function Loading() {
+  // You can add any UI inside Loading, including a Skeleton.
+  return (
+    <div className="flex justify-center items-center text-center my-5">
+      <span>Loading...</span>
+    </div>
+  );
+}
