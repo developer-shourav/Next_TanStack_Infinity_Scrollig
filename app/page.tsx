a33fa629a6a1ea526b21ad4e33289a2900b9d97d
developer-shourav <mrinmoyraj823@gmail.com>
2024-08-07 15:40:47 +0600

    onClick pagination completed

--- app/page.tsx
+++ app/page.tsx
@@ -1,22 +1,46 @@
 "use client";
-import { useEffect, useState } from "react";
+
+import { useState } from "react";
 import TaskCard from "./components/Card/TaskCard";
 import { TaskDataType } from "./Types/task";
+import { useInfiniteQuery } from "@tanstack/react-query";
 
 export default function Home() {
-  const [tasksList, setTasksList] = useState<TaskDataType[]>([]);
 
-  const fetchTask = async () => {
+   const [tasksList, setTasksList] = useState<TaskDataType[]>([]);
+
+  const fetchTask = async ({ pageParam }: { pageParam: number }) => {
     const res = await fetch(
-      "https://jsonplaceholder.typicode.com/todos?_page=1"
+      `https://jsonplaceholder.typicode.com/todos?_page=${pageParam}`
     );
-    const data = await res.json();
-    setTasksList(data);
+
+    return res.json();
   };
 
-  useEffect(() => {
-    fetchTask();
-  }, []);
+  const { data, error, fetchNextPage, status,  } = useInfiniteQuery({
+    queryKey: ["tasksList"],
+    queryFn: fetchTask,
+    initialPageParam: 1,
+
+    getNextPageParam: (lastPage, allPages) => {
+  
+      return allPages?.length + 1;
+    },
+  });
+
+  const content = data?.pages.map((tasks: TaskDataType[]) => 
+    tasks?.map((task) => {
+      return  <TaskCard key={task?.id} task={task} />
+    }) 
+   );
+
+
+  if (status === "pending") {
+    return <p>Loading....</p>;
+  }
+  if (status === "error") {
+    return <p>Error: {error.message}</p>;
+  }
 
   return (
     <main>
@@ -29,10 +53,19 @@ export default function Home() {
         {/* ---------------Main Content---------- */}
         <div className="mt-10 w-10/12 md:w-11/12 xl:w-full mx-auto md:mt-14 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-x-6 gap-y-6 lg:gap-y-10">
           {/* ----------------Task Card ----------------  */}
-          {tasksList?.map((task) => (
-            <TaskCard key={task?.id} task={task} />
-          ))}
+          {content}
+
+           {/* ----------Load More Button----------- */}
+
+        <button
+          onClick={() => fetchNextPage()}
+          className="bg-black text-[14px] w-[110px] h-[40px] rounded-full text-white mt-auto"
+        >
+          Load More...
+        </button>
         </div>
+
+       
       </div>
     </main>
   );
